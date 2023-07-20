94133f7b900307f067a72349fa4b4d67199e6019
saloni-mamtora-22 <saloni.mamtora.dev@gmail.com>
2023-07-20 22:41:36 +0530

    Updates

---
+++ precache-manifest.7eba18fd9dbca3e9855035a32adc59a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc1b79a528b7441de967fc52cf54d29",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "dcecff7a000ba19d1499",
+    "url": "/myportfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c2e9813f585593f0a613",
+    "url": "/myportfolio/static/css/main.a40363da.chunk.css"
+  },
+  {
+    "revision": "dcecff7a000ba19d1499",
+    "url": "/myportfolio/static/js/2.c0b16866.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/myportfolio/static/js/2.c0b16866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e9813f585593f0a613",
+    "url": "/myportfolio/static/js/main.77cf3a43.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2557f007b3e09e3e33af937ac9985a97.js"
+  "/myportfolio/precache-manifest.7eba18fd9dbca3e9855035a32adc59a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
