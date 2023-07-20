9e49dd3a17f0fe149a3e76d349fe94922b43ff8d
Sahil Bhanvadiya <sahilbhanvadiya@Sahils-MacBook-Air.local>
2023-07-20 20:41:57 +0530

    Updates

---
+++ precache-manifest.2557f007b3e09e3e33af937ac9985a97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a67cf72996e963bf5150d8f24f7fdcca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcecff7a000ba19d1499",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c2e9813f585593f0a613",
+    "url": "/static/css/main.a40363da.chunk.css"
+  },
+  {
+    "revision": "dcecff7a000ba19d1499",
+    "url": "/static/js/2.c0b16866.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.c0b16866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e9813f585593f0a613",
+    "url": "/static/js/main.77cf3a43.chunk.js"
+  },
+  {
+    "revision": "e3473a344493b6ab086d",
+    "url": "/static/js/runtime-main.21e4bbbf.js"
+  }
+]);
